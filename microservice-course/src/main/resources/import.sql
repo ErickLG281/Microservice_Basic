2e1e21416535b907913bc8e2212a4f861ded8ae0
ErickLG <132631485+ErickLG281@users.noreply.github.com>
2024-10-25 18:01:38 -0500

    Proyecto Terminado de una Api

---
+++ microservice-course/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+
+insert into courses ( name, teacher  ) values ( 'Matematica', 'Profesor A');
+insert into courses ( name, teacher  ) values ( 'Fisica', 'Profesor B');
+insert into courses ( name, teacher  ) values ( 'Quimica', 'Profesor C');
+insert into courses ( name, teacher  ) values ( 'Biologia', 'Profesor D');
+insert into courses ( name, teacher  ) values ( 'Historia', 'Profesor E');
---
+++ microservice-student/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into students ( name, last_name, email, course_id ) values ( 'John', 'Doe', 'ex@gmail.com', 1 );
+insert into students ( name, last_name, email, course_id ) values ( 'Jane', 'Doe', 'ja@sa.com', 2 );
+insert into students ( name, last_name, email, course_id ) values ( 'Jack', 'Doe', 'sadas@.com', 1 );
+insert into students ( name, last_name, email, course_id ) values ( 'Jill', 'Doe', 'sadas@.com', 3 );
+insert into students ( name, last_name, email, course_id ) values ( 'Joe', 'Doe', 'sadas@.com', 2 );
